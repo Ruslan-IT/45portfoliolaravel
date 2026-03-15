ccbac55ee6ce4faf7c9fe662b052478ed4173b4e
Rusaln-IT <ruslan.r.f@yandex.ru>
2026-03-15 14:53:35 +0500

    ╨а╨░╨╖╨┤╨╡╨╗╨╡╨╜╨╕╨╡ ╨╗╨╛╨│╨╕╨║╨╕ ╤А╨░╨▒╨╛╤В╤Л ╨┐╤А╨╕╨╗╨╛╨╢╨╡╨╜╨╕╤П  Controller тЖТ Service тЖТ Model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,15 +17,7 @@ class HomeController
     {
         $title = 'Catalog';
 
-
         $siteSettings = SiteSetting::first();
-
-        // Получаем случайные категории для блока категорий
-        $categoriesRandom = Category::inRandomOrder()->get();
-
-        // Получаем случайные продукты для блока выгодных предложений (например 5)
-        $deals = Product::inRandomOrder()->take(8)->get();
-        $products = Product::inRandomOrder()->take(30)->get();
 
 
         $sections = Section::where('is_active', true)
@@ -44,10 +36,7 @@ class HomeController
 
         return view('pages.index', compact(
             'title',
-            'categoriesRandom',
-            'deals',
             'siteSettings',
-            'products',
             'homePage',
             'sections',
             'works',
